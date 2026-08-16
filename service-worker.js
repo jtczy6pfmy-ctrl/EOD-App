2c7dc3c3d76869f33657f234ef3d9c9de8bc47dd
jtczy6pfmy-ctrl <jtczy6pfmy@privaterelay.appleid.com>
2026-08-15 22:48:58 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,81 @@
-const CACHE_NAME = "eod-inspection-v3";
+const CACHE_NAME = "eod-inspection-v4";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./manifest.json"
+  "./"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME).then(cache => {
+
       return cache.addAll(FILES_TO_CACHE);
+
     })
+
   );
+
+  self.skipWaiting();
+
 });
 
+
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(keys =>
+
       Promise.all(
+
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
+
       )
+
     )
+
   );
+
+  self.clients.claim();
+
 });
+
 
 self.addEventListener("fetch", event => {
 
-  if (event.request.url.endsWith("/index.html")) {
+  if(
+    event.request.mode === "navigate" ||
+    event.request.url.endsWith("/index.html")
+  ){
+
     event.respondWith(
-      fetch(event.request, { cache: "no-store" })
-        .catch(() => caches.match(event.request))
+
+      fetch(event.request, {
+        cache: "no-store"
+      })
+
+      .catch(() =>
+        caches.match(event.request)
+      )
+
     );
+
     return;
+
   }
 
+
   event.respondWith(
+
     caches.match(event.request).then(cached => {
+
       return cached || fetch(event.request);
+
     })
+
   );
+
 });
